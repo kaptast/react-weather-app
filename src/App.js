e053848e5589fa8f41c72f0f2cbc8e4e3b1a7a62
kaptast <kaptas.tamas@gmail.com>
2021-01-12 22:45:04 +0100

    setup idb

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { DBConfig } from './DBConfig';
-import { initDB } from 'react-indexed-db';
-
 import Dashboard from './components/dashboard/Dashboard';
 import Login from './components/login/Login';
-
-initDB(DBConfig);
+import DBConfig from './DBConfig';
 
 function App() {
+
+  DBConfig();
+
   const [token, setToken] = useState();
 
   if(!token) {
--- src/DBConfig.js
+++ src/DBConfig.js
@@ -1,29 +1,11 @@
-export const DBConfig = {
-    name: 'weather-db',
-    version: 1,
-    objectStoresMeta: [
-      {
-        store: 'users',
-        storeConfig: { keyPath: 'id', autoIncrement: true },
-        storeSchema: [
-          { name: 'username', keypath: 'username', options: { unique: true } },
-          { name: 'password', keypath: 'password', options: { unique: false } }
-        ]
-      },
-      {
-        store: 'cities',
-        storeConfig: { keyPath: 'id', autoIncrement: true },
-        storeSchema: [
-          { name: 'name', keypath: 'username', options: { unique: true } },
-        ]
-      },
-      {
-        store: 'usercities',
-        storeConfig: { keyPath: 'id', autoIncrement: true },
-        storeSchema: [
-          { name: 'userid', keypath: 'userid', options: { unique: false } },
-          { name: 'cityid', keypath: 'cityid', options: { unique: false } }
-        ]
-      }
-    ]
-  };
+import { openDB } from 'idb';
+
+export default async function demo10() {
+    const db = await openDB('weather_db', 1, {
+        upgrade(db) {
+          var userStore = db.createObjectStore('users', { keyPath: 'id', autoIncrement: true});
+          userStore.createIndex('username', 'username', {unique: true});
+          db.createObjectStore('cities', { keyPath: 'id', autoIncrement: true});
+        }
+    });
+}

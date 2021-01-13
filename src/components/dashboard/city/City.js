88680a123215c53e405e8e90aa42e68bc848c386
kaptast <kaptas.tamas@gmail.com>
2021-01-13 17:31:47 +0100

    add delete button

--- src/components/dashboard/city/City.js
+++ src/components/dashboard/city/City.js
@@ -4,6 +4,18 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Gauge from './Gauge';
 import HumidityChart from './HumidityChart';
+import { openDB } from 'idb';
+import { IconButton } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+async function deleteCity(cityid) {
+    const db = await openDB('weather_db', 1);
+    db.delete('cities', cityid)
+        .then(console.log)
+        .catch(err => {
+            console.error(err);
+        });
+}
 
 const defaultData = {
     main: {
@@ -35,14 +47,27 @@ export default function City(props) {
             })
     }, [props.city.cityname]);
 
+    const handleDeleteClick = event => {
+        event.preventDefault();
+        deleteCity(props.city.id);
+        props.setCitiesCallback(true);
+    }
+
     return (
         <Grid container direction="columns">
-            <Grid item>
+            <Grid item container justify="flex-end">
+                <Grid item xs={11}>
                 <Typography variant="h1">
                     {props.city.cityname}
                 </Typography>
+                </Grid>
+                <Grid item>
+                    <IconButton onClick={handleDeleteClick}>
+                        <DeleteIcon />
+                    </IconButton>
+                </Grid>
             </Grid>
-            <Grid item container direction="row">
+            <Grid item container direction="row" spacing={2} justify="center">
                 <Gauge name={"Temperature"} value={currentWeather.main.temp} unit={"°C"} />
                 <Gauge name={"Humidity"} value={currentWeather.main.humidity} unit={"%"} />
                 <Gauge name={"Pressure"} value={currentWeather.main.pressure} unit={"hPa"} />

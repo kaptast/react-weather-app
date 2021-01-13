8e2c441b235804ae9b0a4708a6ce9c66a72c048c
kaptast <kaptas.tamas@gmail.com>
2021-01-13 11:10:49 +0100

    set db schema

--- src/DBConfig.js
+++ src/DBConfig.js
@@ -1,11 +1,13 @@
 import { openDB } from 'idb';
 
-export default async function demo10() {
-    const db = await openDB('weather_db', 1, {
+export function DBConfig() {
+    openDB('weather_db', 1, {
         upgrade(db) {
           var userStore = db.createObjectStore('users', { keyPath: 'id', autoIncrement: true});
           userStore.createIndex('username', 'username', {unique: true});
-          db.createObjectStore('cities', { keyPath: 'id', autoIncrement: true});
+
+          var citiesStore = db.createObjectStore('cities', { keyPath: 'id', autoIncrement: true});
+          citiesStore.createIndex('userid', 'userid');
         }
-    });
+    }).then();
 }

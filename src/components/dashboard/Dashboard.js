0bc024f238dca5321dd5cb0568c6334ca9a52995
kaptast <kaptas.tamas@gmail.com>
2021-01-13 13:19:25 +0100

    query cities in dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import CityList from './CityList';
+import { openDB } from 'idb';
 
 export default function Dashboard({ logoutCallback, userid }) {
+    const [cities, setCities] = useState([]);
+
+    useEffect(() => {
+
+        openDB('weather_db', 1).then(db => {
+            console.log("querying cities");
+            console.log(userid);
+            db.getAllFromIndex('cities', 'userid', userid)
+                .then(userCities => {
+                    console.log(userCities);
+                    if (Array.isArray(userCities)) {
+                        console.log('setting cities');
+                        setCities(userCities);
+                    }
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        })
+
+    }, [userid]);
+
     return (
         <CityList cities={cities} userid={userid} logoutCallback={logoutCallback} />
     );
